921d42c140708142c32d0669b8a4b049deaeaf4d
LZephyr <hzlvzhanfeng@corp.netease.com>
2017-09-26 17:33:27 +0800

    整理代码

---
+++ Mapper/ASTNode/InvokeNode.swift
@@ -0,0 +1,14 @@
+//
+//  InvokeNode.swift
+//  Mapper
+//
+//  Created by LZephyr on 2017/9/26.
+//  Copyright © 2017年 LZephyr. All rights reserved.
+//
+
+import Cocoa
+
+class InvokeNode: Node {
+    var methodName: String = ""
+    var paramList: [String: String] = [:]
+}
