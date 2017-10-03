2fd6b7b11e4474d8c34dcbae4938c2daebff630e
LZephyr <lzf.lzephyr@gmail.com>
2017-10-03 20:36:59 +0800

    InterfaceParser继承自BacktrackParser

---
+++ Drafter/Parser/Swift/SwiftClassParser.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftClassParser.swift
+//  Drafter
+//
+//  Created by LZephyr on 2017/10/3.
+//  Copyright © 2017年 LZephyr. All rights reserved.
+//
+
+import Foundation
+
+class SwiftClassParser: BacktrackParser {
+    
+}
