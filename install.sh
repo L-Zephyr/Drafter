9b00ac3554234b75b40eac0cc56ec227d5d5c08d
LZephyr <lzf.lzephyr@gmail.com>
2019-06-01 18:09:59 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 cd ~/
 git clone https://github.com/L-Zephyr/Drafter.git
 cd Drafter
-swift build -c release -Xswiftc -static-stdlib
+
+# check if static link is needed
+if [ -d "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift_static/macosx" ];then
+    swift build -c release -Xswiftc -static-stdlib
+else
+    swift build -c release
+fi
+
 unzip -o ./Template/template.zip -d ./Template/drafter
 cp -f -r ./Template/drafter/template/ ~/.drafter
 cd .build/release
